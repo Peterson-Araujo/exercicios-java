d7a1c9f69b73c2e85c3c0539865c80ede0e35e64
Peterson Araujo <petersonaraujo70@gmail.com>
2025-10-14 16:42:31 -0300

    01 - Estoque médio (Competo)

---
+++ EstoqueMedio.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class EstoqueMedio {
+
+    // Método para calcular e retornar o estoque médio
+    public double calcularEstoqueMedio(int quantidadeMinima, int quantidadeMaxima) {
+        return (quantidadeMinima + quantidadeMaxima) / 2.0;
+    }
+
+    // Método principal
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        EstoqueMedio estoque = new EstoqueMedio();
+
+        System.out.println("..:: Cálculo de Estoque Médio ::..");
+
+        System.out.print("Digite a quantidade mínima de itens: ");
+        int quantidadeMinima = sc.nextInt();
+
+        System.out.print("Digite a quantidade máxima de itens: ");
+        int quantidadeMaxima = sc.nextInt();
+
+        if (quantidadeMinima < 0 || quantidadeMaxima < 0) {
+            System.out.println("Os valores não podem ser negativos.");
+        } else if (quantidadeMaxima < quantidadeMinima) {
+            System.out.println("A quantidade máxima deve ser maior ou igual à mínima.");
+        } else {
+            double resultado = estoque.calcularEstoqueMedio(quantidadeMinima, quantidadeMaxima);
+            System.out.printf("O estoque médio do item é de: %.2f%n", resultado);
+        }
+
+        sc.close();
+    }
+}
+
